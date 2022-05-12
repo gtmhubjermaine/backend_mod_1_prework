32577d8c3d872e7705248ddb5b3ab838b5a54b4e
Jermaine Braumuller <jermaine@gtmhub.com>
2022-05-12 12:13:29 +0200

    final prep

--- section2/exercises/methods.rb
+++ section2/exercises/methods.rb
@@ -12,7 +12,7 @@ print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  p name
 end
 
 print_name("Albus Dumbledore")
@@ -20,12 +20,20 @@ print_name("Albus Dumbledore")
 # Write a method that takes in 2 numbers as arguments and prints 
 # their sum. Then call your method three times with different arguments passed in:
 # YOUR CODE HERE
+def add(num, num2)
+  num + num2
+end
 
+p add(2,2)
 # Write a method that takes in two strings as arguments and prints 
 # a concatenation of those two strings. Example: The arguments could be 
 # (man, woman) and the end result might output: "When Harry Met Sally".  
 # Then call your method three times with different arguments passed in. 
+def name(num, num2)
+  num + num2
+end
 
+p add("jay", "dilla")
 
 #-------------------
 # PART 3: Naming is Hard
